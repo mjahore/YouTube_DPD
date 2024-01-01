286cbcf644003a90c7cec49fb3a771a560032478
Mike Hore <hore@case.edu>
2024-01-01 15:04:42 -0500

    Initial Commit

---
+++ dpd.c
@@ -0,0 +1,23 @@
+/***********************************************************
+ *
+ * dpd.c - Example DPD simulation of a polymer in solution
+ *         from YouTube video series available at:
+ *         https://www.xxxxxx.xxx
+ *
+ * Author: Michael J. A. Hore (hore@case.edu)
+ *
+ *
+ ***********************************************************/
+ 
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include "cell_list.h"
+
+int main(int argc, char *argv[]) {
+
+
+
+	return 0;
+}
